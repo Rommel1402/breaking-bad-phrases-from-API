6736d633d55ff85c40479be9a332af6d5afc2d3a
unknown <rommel.sanzonetti@correo.unimet.edu.ve>
2020-12-23 14:05:16 -0400

    it's hosted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import Frase from './components/Frase';
+
+
+const Boton= styled.button`
+
+  background:-webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size:300px;
+  font-family: Arial, Helvetica, sans-serif, sans-serif;
+  color:#fff;
+  margin-top: 3rem;
+  padding:1rem 3rem;
+  font-size:2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  :hover{
+    cursor:pointer;
+    background-size:400;
+  }
+
+`;
+
+const Contenedor=styled.div`
+
+  display:flex;
+  align-items:center;
+  padding-top:5rem;
+  flex-direction:column;
+
+`;
+
 
 function App() {
+  
+  // const consultarAPI=()=>{
+  //   // console.log('consultando...');
+  //   const api=fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+  //   // console.log(resultado);
+  //   const frase=api.then(respuesta=>respuesta.json());
+  //   frase.then(resultado=> console.log(resultado));
+  //   console.log(frase);
+
+  // }
+
+  //state de frases
+  const [frase, ObtenerFrase]= useState({});
+
+
+  //Cargar una frase
+  useEffect(()=>{
+    consultarAPI();
+  },[]);
+
+  const consultarAPI=async()=>{
+    const api=await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase=await api.json();
+    // console.log(frase);
+    // console.log(frase[0]);
+    ObtenerFrase(frase[0]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+    <Frase
+      frase={frase}
+    />
+      <Boton
+        onClick={consultarAPI}
+      >Obtener Frase</Boton>
+    </Contenedor>
+
   );
 }
 
